8e73816a1bbbf654c672407d7d4dd3b0cbc502f9
Eivind Fonn <evfonn@gmail.com>
2013-01-25 14:46:55 +0100

    Fixed backup.

--- aligulac/dump.py
+++ aligulac/dump.py
@@ -14,7 +14,7 @@ files = [f.strip() for f in files if f.strip() != '']
 files.append('%s.sql' % dt.isoformat())
 
 if len(files) > 40:
-    os.system('rm /usr/local/www/aligulac/backup/%s.sql' % files[0])
+    os.system('rm /usr/local/www/aligulac/backup/%s' % files[0])
     files = files[1:]
 
 with open('/usr/local/www/aligulac/backup/files', 'w') as f:

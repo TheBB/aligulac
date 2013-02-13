3e1769d307f6950eef57eae16068450fbc20e264
Eivind Fonn <evfonn@gmail.com>
2013-02-13 22:29:33 +0100

    Added switch_to

--- templates/js/general.js
+++ templates/js/general.js
@@ -41,6 +41,19 @@ function togvis_div(id)
     togvis(id, 'block');
 }
 
+/* Makes all elements given in the all array invisible except for the one given by id
+ */
+function switch_to(id, all)
+{
+    for (i = 0; i < ids.length; i++)
+    {
+        if (all[i] == id)
+            document.getElementById(all[i]).style.display = 'block';
+        else
+            document.getElementById(all[i]).style.display = 'none';
+    }
+}
+
 /* ======================================================================
  * TEXT TOGGLING
  * ======================================================================

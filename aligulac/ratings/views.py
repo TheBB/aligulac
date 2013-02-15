86d1567a7dd939993813955498c83efeb791cbbe
Eivind Fonn <evfonn@gmail.com>
2013-02-15 12:56:15 +0100

    More modifications in res search.

--- aligulac/ratings/views.py
+++ aligulac/ratings/views.py
@@ -411,6 +411,10 @@ def results_search(request):
                     match.eventobj = event
                 if request.POST['date'].strip() != '':
                     match.date = request.POST['date']
+                if request.POST['type'] != 'nochange':
+                    match.offline = (request.POST['type'] == 'offline')
+                if request.POST['game'] != 'nochange':
+                    match.game = request.POST['game']
                 match.save()
                 num += 1
 

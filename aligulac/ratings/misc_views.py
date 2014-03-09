60474655d37f51e7cc28bc12f8c589097fae4af1
Rasmus Précenth <rprecenth@gmail.com>
2014-03-09 18:18:06 +0100

    Much improve, wow

--- aligulac/ratings/misc_views.py
+++ aligulac/ratings/misc_views.py
@@ -1,5 +1,6 @@
 # {{{ Imports
 from collections import namedtuple
+from datetime import datetime
 
 from django.core.exceptions import PermissionDenied
 from django.db.models import F, Q
@@ -7,6 +8,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 
 from ratings.models import (
+    Event,
     Group,
     Match,
     Player,
@@ -20,23 +22,41 @@ from ratings.templatetags.ratings_extras import (
 # }}}
 
 # {{{
-# Format (description, queryset, type)
-Clock = namedtuple('Clock', ['desc', 'object', 'type', 'date', 'extra'])
+# Format (description, hover_description, queryset, type)
+Clock = namedtuple('Clock', ['desc', 'alt_desc', 'object', 'type', 'date', 'extra'])
 CLOCKS = [
     (
         "MMA and DongRaeGu played a Bo5+",
-        Match.objects.symmetric_filter(pla_id=28, plb_id=4)\
-        .filter(Q(sca__gte=3)|Q(scb__gte=3)).order_by("-date"),
+        None,
+        (
+            Match.objects
+            .symmetric_filter(pla_id=28, plb_id=4)
+            .filter(Q(sca__gte=3) | Q(scb__gte=3))
+            .order_by("-date")
+        ),
         "match"
     ),
     (
-        "Mvp won a Bo7",
-        Match.objects.symmetric_filter(pla_id=13, sca=4, sca__gte=F("scb"))\
-        .order_by("-date"),
+        "Mvp won a premier event",
+        None,
+        Event.objects.filter(type="event",
+                             earnings__player_id=13,
+                             earnings__placement=1,
+                             earnings__earnings__gte=10000)
+        .order_by("-latest"),
+        "event_winner"
+    ),
+    (
+        "A korean terran won against a korean protoss in a Bo5+ (offline)",
+        None,
+        Match.objects.symmetric_filter(
+            Q(pla__country="KR", rca="P", scb__gt=F("sca"), rcb="T", plb__country="KR")
+        ).filter(Q(sca__gte=3) | Q(scb__gte=3), offline=True).order_by("-date"),
         "match"
     ),
     (
         "A foreign terran won against a korean protoss (offline)",
+        None,
         Match.objects.symmetric_filter(
             Q(pla__country="KR", rca="P", scb__gt=F("sca"), rcb="T") & ~Q(plb__country="KR")
         ).filter(offline=True).order_by("-date"),
@@ -44,17 +64,73 @@ CLOCKS = [
     ),
     (
         "A foreigner won in ProLeague",
-        Match.objects.symmetric_filter(~Q(pla__country="KR") & Q(sca__gt=F("scb")))\
-        .filter(eventobj__fullname__istartswith="ProLeague")\
+        None,
+        Match.objects.symmetric_filter(~Q(pla__country="KR") & Q(sca__gt=F("scb")))
+        .filter(eventobj__fullname__istartswith="ProLeague")
         .order_by("-date"),
         "match"
     ),
     (
         "A foreginer won in GSL Code S",
-        Match.objects.symmetric_filter(~Q(pla__country="KR") & Q(sca__gt=F("scb")))\
-        .filter(Q(eventobj__fullname__istartswith="GSL", eventobj__fullname__icontains="Code S"))\
+        None,
+        Match.objects.symmetric_filter(~Q(pla__country="KR") & Q(sca__gt=F("scb")))
+        .filter(eventobj__fullname__istartswith="GSL", eventobj__fullname__icontains="Code S")
         .order_by("-date"),
         "match"
+    ),
+    (
+        "A CIS player won a major event",
+        "Player from BY, RU, UA or KZ with 1st place prize money >= $2000",
+        Event.objects.filter(earnings__placement=1,
+                             earnings__player__country__in=["BY","RU","UA","KZ"],
+                             earnings__earnings__gte=2000)
+        .order_by("-latest"),
+        "event_winner"
+    ),
+    (
+        "A Nordic player won a major event",
+        "Player from SE, NO, DK, IS or FI with 1st place prize money >= $2000",
+        Event.objects.filter(earnings__placement=1,
+                             earnings__player__country__in=["SE", "NO", "FI"],
+                             earnings__earnings__gte=2000)
+        .order_by("-latest"),
+        "event_winner"
+    ),
+    (
+        "A North American player won a major event",
+        "Player from US or CA with 1st place prize money >= $2000",
+        Event.objects.filter(earnings__placement=1,
+                             earnings__player__country__in=["US", "CA"],
+                             earnings__earnings__gte=2000)
+        .order_by("-latest"),
+        "event_winner"
+    ),
+    (
+        "A foreginer won a premier event",
+        "At least on game played offline with 1st place prize money >= $10,000",
+        (
+            Event.objects
+            .filter(type="event")
+            .filter(downlink__child__match__offline=True)
+            .filter(~Q(earnings__player__country="KR"),
+                    earnings__placement=1,
+                    earnings__earnings__gte=10000)
+            .distinct()
+            .order_by("-latest")
+        ),
+        "event_winner"
+    ),
+    (
+        "JaeDong got second place in an event",
+        None,
+        (
+            Event.objects
+            .filter(type="event")
+            .filter(earnings__player_id=73,
+                    earnings__placement=2)
+            .order_by("-latest")
+        ),
+        "event_winner"
     )
 ]
 
@@ -63,7 +139,7 @@ def clocks(request):
 
     ctx["title"] = "Number of days since..."
     ctx["clocks"] = list()
-    for desc, q, t in CLOCKS:
+    for desc, alt_desc, q, t in CLOCKS:
         obj = None
         extra = None
         date = None
@@ -72,17 +148,31 @@ def clocks(request):
             q = q.prefetch_related("pla", "plb", "eventobj")
             matches = list(q[:10])
 
-            if len(matches) > 1:
-                extra = display_matches([matches[0]]), display_matches(matches[1:10])
-            else:
-                extra = display_matches([matches[0]]), None
+            extra = display_matches(matches)
 
             obj = matches[0]
             date = obj.date
 
-        c = Clock(desc, obj, t, date.strftime("%Y-%m-%d"), extra)
+        elif t == "event_winner":
+            q = q.prefetch_related("earnings_set")
+            events = list(q[:10])
+            obj = events[0]
+            extra = list()
+            for e in events:
+                pearnings = list(
+                    e.earnings_set
+                    .exclude(placement=0)
+                    .order_by("placement")
+                    .prefetch_related("player")[:2]
+                )
+                extra.append((e, pearnings))
+
+            date = obj.latest
+
+        c = Clock(desc, alt_desc, obj, t, date.strftime("%Y-%m-%d"), extra)
 
         ctx["clocks"].append(c)
+        ctx["clocks"].sort(key=lambda c: -datetime.strptime(c.date, "%Y-%m-%d").timestamp())
 
     return render_to_response("clocks.html", ctx)
 # }}}

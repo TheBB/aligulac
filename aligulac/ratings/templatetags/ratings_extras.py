332a386d71461838db37f991033c9acb6aa0991e
Eivind Fonn <evfonn@gmail.com>
2014-04-11 18:52:45 +0200

    Double and triple arrows
    Fixes #154

--- aligulac/ratings/templatetags/ratings_extras.py
+++ aligulac/ratings/templatetags/ratings_extras.py
@@ -1,4 +1,3 @@
-# {{{ Imports
 import hashlib
 import markdown2
 from math import sqrt
@@ -35,27 +34,48 @@ from ratings.models import (
     TLPD_DB_HOTSBETA,
     TLPD_DB_WOLBETA,
 )
-# }}}
     
 register = template.Library()
 
-# {{{ Miscellaneous filters
 
-# {{{ markdown: Filters text written in markdown.
+# signifiy: Put a + in front if positive, = if zero
+@register.filter
+def signify(value):
+    if value < 0:
+        return '–' + str(abs(value))
+    elif value > 0:
+        return '+' + str(value)
+    else:
+        return '='
+
+# makearrows: Takes a rating difference and outputs an arrow image
+@register.filter
+def makearrows(value):
+    if value == 0:
+        return 'trans'
+
+    if abs(value) > 0.1:
+        strength = '3'
+    elif abs(value) > 0.04:
+        strength = '2'
+    else:
+        strength = '1'
+    return ('up' if value > 0 else 'down') + 'arrow' + strength
+
+# markdown: Filters text written in markdown.
 @register.filter
 @stringfilter
 def markdown(value):
     return mark_safe(markdown2.markdown(value, safe_mode=True))
-# }}}
 
-# {{ jsescape: Escapes a string to be placed inside a
+# jsescape: Escapes a string to be placed inside a
 #              javascript string
 @register.filter
 @stringfilter
 def jsescape(value):
     return value.replace(r"'", r"\'").replace(r'"', r'\"')
 
-# {{{ urlify: Adds links to URLs.
+# urlify: Adds links to URLs.
 @register.filter
 @stringfilter
 def urlify(value):
@@ -71,46 +91,39 @@ def urlify(value):
     value = pat1.sub(r'\1<a href="\2">\3</a>', value)
     value = pat2.sub(r'\1<a href="http://\2">\3</a>', value)
     return mark_safe(value)
-# }}}
 
-# {{{ player_url: Generate a player URL.
+# player_url: Generate a player URL.
 @register.filter
 def player_url(value):
     return "/players/" + str(value.id) + "-" + urlfilter(value.tag) + "/"
-# }}}
 
-# {{{ vs_url: Generate a search query for the two players
+# vs_url: Generate a search query for the two players
 @register.filter
 def vs_url(value, arg):
     return "/results/search/?search=&after=&before=&players={}%0D%0A{}" \
         "&event=&bestof=all&offline=both&game=all&op=Search".format(value, arg)
-# }}}
 
-# {{{ addf: Adds floating point numbers.
+# addf: Adds floating point numbers.
 @register.filter
 def addf(value, arg):
     return float(value) + float(arg)
-# }}}
 
-# {{{ subf: Subtracts floating point numbers.
+# subf: Subtracts floating point numbers.
 @register.filter
 def subf(value, arg):
     return float(value) - float(arg)
-# }}}
 
-# {{{ sub: Subtracts integers.
+# sub: Subtracts integers.
 @register.filter
 def sub(value, arg):
     return int(value) - int(arg)
-# }}}
 
-# {{{ Integer exponentiation.
+# Integer exponentiation.
 @register.filter
 def pow(value, arg):
     return int(value) ** int(arg)
-# }}}
 
-# {{{ racefull: Converts a single-character race to human readable format.
+# racefull: Converts a single-character race to human readable format.
 @register.filter
 def racefull(value):
     return ([
@@ -120,9 +133,8 @@ def racefull(value):
         _('Random'),
         _('Race switcher')][['P','T','Z','R','S'].index(value)]
     )
-# }}}
 
-# {{{ haslogo: Checks whether a team given by ID has a logo file.
+# haslogo: Checks whether a team given by ID has a logo file.
 @register.filter
 def haslogo(value):
     try:
@@ -130,9 +142,8 @@ def haslogo(value):
             return True
     except:
         return False
-# }}}
 
-# {{{ Creates match prediction links.
+# Creates match prediction links.
 @register.filter
 def makematchlink(value):
     if value['pla']['id'] is None or value['plb']['id'] is None:
@@ -146,18 +157,15 @@ def makematchlink(value):
             value['pla']['score'],
             value['plb']['score'],
     ))
-# }}}
 
-# {{{ milliseconds: Converts a date (not datetime) to millisecond format, needed for using the charts.
+# milliseconds: Converts a date (not datetime) to millisecond format, needed for using the charts.
 @register.filter
 def milliseconds(value):
     return (value - date(1970,1,1)).days * 24 * 60 * 60 * 1000
-# }}}
 
-# {{{ add_separator: Adds separators to a large number.
+# add_separator: Adds separators to a large number.
 @register.filter
 def add_separator(value):
-
     svalue = str(value)
 
     if '.' in svalue:
@@ -176,9 +184,8 @@ def add_separator(value):
         else:
             newstring = ',' + string[-3:] + newstring
             string = string[:-3]
-# }}}
 
-# {{{ add_sep_and_cur: Adds separators and currency symbol to a prize sum.
+# add_sep_and_cur: Adds separators and currency symbol to a prize sum.
 @register.filter
 def add_sep_and_cur(value, cur):
     s = add_separator(value)
@@ -210,23 +217,20 @@ def add_sep_and_cur(value, cur):
     #     return "\u0243 " + s
     else:
         return s + " " + cur
-# }}}
 
-# {{{ is_false: Checks for identity (not just equality) with False.
+# is_false: Checks for identity (not just equality) with False.
 @register.filter
 def is_false(b):
     return b is False
-# }}}
 
-# {{{ smallhash: Generates a small (6-character) string hash.
+# smallhash: Generates a small (6-character) string hash.
 @register.filter
 def smallhash(value):
     m = hashlib.md5()
     m.update(value.encode('utf-8'))
     return m.hexdigest()[:6]
-# }}}
 
-# {{{ makedate: Produces a date in the right format, or none.
+# makedate: Produces a date in the right format, or none.
 @register.filter
 def makedate(value):
     if isinstance(value, str):
@@ -235,9 +239,8 @@ def makedate(value):
         except:
             value = None
     return value
-# }}}
 
-# {{{ get_tlpd_list: Takes a TLPD DB bit-flag integer and returns a list of databases.
+# get_tlpd_list: Takes a TLPD DB bit-flag integer and returns a list of databases.
 @register.filter
 def get_tlpd_list(value):
     value = int(value) if value is not None else 0
@@ -255,12 +258,10 @@ def get_tlpd_list(value):
         if value & db_flag != 0:
             ret.append((tlpd_db_name, human_db_name))
     return ret
-# }}}
-# }}}
 
-# {{{ URL generation filters
+# URL generation filters
 
-# {{{ css: Generates a CSS file URL
+# css: Generates a CSS file URL
 @register.filter
 @stringfilter
 def css(value):
@@ -268,9 +269,8 @@ def css(value):
         return 'http://css.aligulac.com/' + value + '.css'
     else:
         return '/css/' + value + '.css'
-# }}}
 
-# {{{ js: Generates a JS file URL
+# js: Generates a JS file URL
 @register.filter
 @stringfilter
 def js(value):
@@ -278,9 +278,8 @@ def js(value):
         return 'http://js.aligulac.com/' + value + '.js'
     else:
         return '/js/' + value + '.js'
-# }}}
 
-# {{{ img: Generates a png-image file URL
+# img: Generates a png-image file URL
 @register.filter
 @stringfilter
 def img(value, folder=None):
@@ -292,17 +291,15 @@ def img(value, folder=None):
         return 'http://img.aligulac.com/' + img_file + '.png'
     else:
         return '/img/' + img_file + '.png'
-# }}}
 
-# {{{ static: Generates URL for static files (must include extension)
+# static: Generates URL for static files (must include extension)
 @register.filter
 @stringfilter
 def static(value):
     return 'http://static.aligulac.com/' + value
 register.filter('static', static)
-# }}}
 
-# {{{ imgdir: Gets the url of the image directory
+# imgdir: Gets the url of the image directory
 # For example {{ '/'|imgdir }} gives the root dir containing images and
 #             {{ '/flags/'|imgdir }} gives the dir containing flags.
 @register.filter
@@ -315,34 +312,28 @@ def imgdir(value):
         return 'http://img.aligulac.com' + value
     else:
         return '/img' + value
-# }}}
 
-# {{{ urlfilter: Generates URL-safe strings for player, team and event names, etc.
+# urlfilter: Generates URL-safe strings for player, team and event names, etc.
 @register.filter
 @stringfilter
 def urlfilter(value):
     value = value.replace(' ', '-')
     value = value.replace('/', '')
     return value
-# }}}
 
-# }}}
+# Rating number display filters
 
-# {{{ Rating number display filters
-
-# {{{ ratscale: Scales a rating number to human-readable format
+# ratscale: Scales a rating number to human-readable format
 @register.filter
 def ratscale(value):
     return int(round((float(value) + 1.0)*1000))
-# }}}
 
-# {{{ ratscalediff: Scales a rating number difference to human-readable format
+# ratscalediff: Scales a rating number difference to human-readable format
 @register.filter
 def ratscalediff(value):
     return int(round(float(value)*1000))
-# }}}
 
-# {{{ ratscaleplus: Like ratscale, but takes infinities and N/A into account (for performances)
+# ratscaleplus: Like ratscale, but takes infinities and N/A into account (for performances)
 @register.filter
 def ratscaleplus(value):
     if value <= PRF_MININF:
@@ -353,19 +344,15 @@ def ratscaleplus(value):
         return _('N/A')
     else:
         return ratscale(value)
-# }}}
 
-# {{{ devrange: Computes RD against a certain race.
+# devrange: Computes RD against a certain race.
 @register.filter
 def ratingdev(event, race):
     return event.get_totaldev(race)
-# }}}
 
-# }}}
+# Percentage filters
 
-# {{{ Percentage filters
-
-# {{{ pctg_add: Percentage of value to value+arg.
+# pctg_add: Percentage of value to value+arg.
 @register.filter
 def pctg_add(value, arg):
     if float(value) + float(arg) > 0:
@@ -379,15 +366,13 @@ def pctg_add_f(value, arg):
         return 100*float(value)/(float(value)+float(arg))
     else:
         return 0
-# }}}
 
-# {{{ pctg_one: Percentage of value to 1.
+# pctg_one: Percentage of value to 1.
 @register.filter
 def pctg_one(value):
     return '%5.2f' % (100*float(value))
-# }}}
 
-# {{{ pctg: Percentage of value to arg.
+# pctg: Percentage of value to arg.
 @register.filter
 def pctg(value, arg):
     if float(arg) > 0:
@@ -401,13 +386,10 @@ def pctg_scl(value, arg):
         return '%.2f' % (100*float(value)/(1.0001*float(arg)))
     else:
         return '%.2f' % 0.0
-# }}}
 
-# }}}
+# Filters to use with OP/UP races
 
-# {{{ Filters to use with OP/UP races
-
-# {{{ oprace: Gets the OP race of a period.
+# oprace: Gets the OP race of a period.
 @register.filter
 def oprace(value):
     if value.dom_p > value.dom_t and value.dom_p > value.dom_z:
@@ -416,9 +398,8 @@ def oprace(value):
         return 'T'
     else:
         return 'Z'
-# }}}
 
-# {{{ uprace: Gets the UP race of a period.
+# uprace: Gets the UP race of a period.
 @register.filter
 def uprace(value):
     if value.dom_p < value.dom_t and value.dom_p < value.dom_z:
@@ -427,67 +408,56 @@ def uprace(value):
         return 'T'
     else:
         return 'Z'
-# }}}
 
-# {{{ oppctg: Gets the percentage of OP-ness of a period.
+# oppctg: Gets the percentage of OP-ness of a period.
 @register.filter
 def oppctg(value):
     return int(round(100*(max([value.dom_p, value.dom_t, value.dom_z]) - 1.)))
-# }}}
 
-# {{{ uppctg: Gets the percentage of UP-ness of a period.
+# uppctg: Gets the percentage of UP-ness of a period.
 @register.filter
 def uppctg(value):
     return int(round(100*(1. - min([value.dom_p, value.dom_t, value.dom_z]))))
-# }}}
 
-# }}}
+# Date filters
 
-# {{{ Date filters
-
-# {{{ tomorrow: Gets the next day.
+# tomorrow: Gets the next day.
 @register.filter
 def tomorrow(value):
     return value + timedelta(1)
-# }}}
 
-# {{{ yesterday: Gets the previous day.
+# yesterday: Gets the previous day.
 @register.filter
 def yesterday(value):
     return value - timedelta(1)
-# }}}
 
-# {{{ nextmonth: Skips one month forward.
+# nextmonth: Skips one month forward.
 @register.filter
 def nextmonth(value):
     return value + relativedelta(months=1)
-# }}}
 
-# {{{ prevmonth: Skips one month back.
+# prevmonth: Skips one month back.
 @register.filter
 def prevmonth(value):
     return value - relativedelta(months=1)
-# }}}
 
-# {{{ datemax: Finds the latest of two dates.
+# datemax: Finds the latest of two dates.
 @register.filter
 def datemax(value, arg):
     if value - arg > timedelta(0):
         return value
     else:
         return arg
-# }}}
 
-# {{{ datemin: Finds the earliest of two dates.
+# datemin: Finds the earliest of two dates.
 @register.filter
 def datemin(value, arg):
     if value - arg < timedelta(0):
         return value
     else:
         return arg
-# }}}
 
-# {{{ cdate: Custom translate-friendly date formatting.
+# cdate: Custom translate-friendly date formatting.
 @register.filter
 def cdate(value, arg):
     temp = (djangodate(value, arg)
@@ -538,13 +508,10 @@ def cdate(value, arg):
         .replace('sun',        ugettext('sun'))
     )
     return temp
-# }}}
 
-# }}}
+# Event-related filters
 
-# {{{ Event-related filters
-
-# {{{ unfold: Returns -value times </div>. (Just... read the code.)
+# unfold: Returns -value times </div>. (Just... read the code.)
 @register.filter
 def unfold(value):
     value = -int(value)
@@ -552,9 +519,8 @@ def unfold(value):
     for i in range(0,value):
         q += '</div>'
     return q
-# }}}
 
-# {{{ indent: Returns 4 x value times &nbsp;
+# indent: Returns 4 x value times &nbsp;
 @register.filter
 def indent(value):
     value = int(value) - 1
@@ -564,9 +530,8 @@ def indent(value):
     for i in range(0,int(value)):
         q += '&nbsp;&nbsp;&nbsp;&nbsp;'
     return q
-# }}}
 
-# {{{ getN: Takes a list of strings and finds the maximal N so that the concatenation of the last N elements
+# getN: Takes a list of strings and finds the maximal N so that the concatenation of the last N elements
 # (interspersed with commas) has length less than 60.
 @register.filter
 def getN(lst):
@@ -575,27 +540,22 @@ def getN(lst):
     while N < len(lst) and sum([2+len(x.name) for x in lst[-N-1::]]) < K:
         N += 1
     return N 
-# }}}
 
-# {{{ eventliststart: Returns the first part of a list split by the strategy described by getN.
+# eventliststart: Returns the first part of a list split by the strategy described by getN.
 @register.filter
 def eventliststart(value, N=None):
     if N == None:
         N = getN(list(value))
     return list(value)[0:-N]
-# }}}
 
-# {{{ eventlistend: Returns the last part of a list split by the strategy described by getN.
+# eventlistend: Returns the last part of a list split by the strategy described by getN.
 @register.filter
 def eventlistend(value, N=None):
     if N == None:
         N = getN(list(value))
     return list(value)[-N:]
-# }}}
 
-# }}}
-
-# {{{ Model display filters
+# Model display filters
 
 @register.filter
 def player(value):
@@ -625,5 +585,3 @@ def event(value):
         "</a>").format(id=value.id,
                        name=value.fullname,
                        safename=urlfilter(value.fullname)))
-
-# }}}

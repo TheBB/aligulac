f3b1989458dee10b66e1acf440e5c859779d1ca8
Eivind Fonn <evfonn@gmail.com>
2014-06-23 15:53:03 +0200

    We don't need list of mini-URLs

--- aligulac/miniURL/urls.py
+++ aligulac/miniURL/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import (
 )
 
 urlpatterns = patterns('miniURL.views',
-    url(r'^$', 'list', name='url_list'),
     url(r'^new/$', 'new', name='url_new'),
     url(r'^(?P<code>\w{16})/$', 'find_redirect', name='url_redirect'),
 )

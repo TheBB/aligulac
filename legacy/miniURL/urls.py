563548feda6eee51f5a0d553f304c80b450de45f
Eivind Fonn <evfonn@gmail.com>
2025-11-14 13:46:06 +0100

    Remove dead code

--- legacy/miniURL/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import new, find_redirect
-
-urlpatterns = [
-    path('new', new, name='url_new'),
-    path('<slug:code>', find_redirect, name='url_redirect'),
-]
